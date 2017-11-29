f2b576d456d9a3545c83a9a479a34e8992225201
danieljaller <danieljaller@gmail.com>
2017-11-29 13:37:31 +0100

    Added dbContext and ConnectionString

---
+++ Kbry.Data/KbryDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kbry.Data.Model;
+
+namespace Kbry.Data
+{
+    class KbryDbContext : DbContext
+    {
+        public KbryDbContext() : base("KbryDB")
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
