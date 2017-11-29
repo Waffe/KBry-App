9c102c1dd330cf90796d8398182d66279d24d63c
danieljaller <danieljaller@gmail.com>
2017-11-29 13:36:39 +0100

    Added models

---
+++ Kbry.Data/Model/Attendance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbry.Data.Model
+{
+    class Attendance
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ Kbry.Data/Model/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbry.Data.Model
+{
+    public class Class
+    {
+        public Class() => Students = new Collection<Student>();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
