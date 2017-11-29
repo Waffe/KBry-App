c2fe8ff280099536fdcdda579b46e9e07d10f812
danieljaller <danieljaller@gmail.com>
2017-11-29 14:35:46 +0100

    Added collection of Attendances to Student

--- Kbry.Data/Model/Student.cs
+++ Kbry.Data/Model/Student.cs
@@ -1,12 +1,17 @@
-﻿namespace Kbry.Data.Model
+﻿using System.Collections.ObjectModel;
+
+namespace Kbry.Data.Model
 {
     public class Student
     {
+        public Student() => Attendances = new Collection<Attendance>();
+
         public int Id { get; set; }
         public string RegistrationCode { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public virtual Class Class { get; set; }
+        public virtual Collection<Attendance> Attendances { get; set; }
     }
 }
