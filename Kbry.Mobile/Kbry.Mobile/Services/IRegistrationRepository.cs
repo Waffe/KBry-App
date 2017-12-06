dfac09f6de77ee723c41453e9597cb5502c525f0
danieljaller <danieljaller@gmail.com>
2017-12-06 12:35:50 +0100

    Implemented functionality for RegistrationRepository.

--- Kbry.Mobile/Kbry.Mobile/Services/IRegistrationRepository.cs
+++ Kbry.Mobile/Kbry.Mobile/Services/IRegistrationRepository.cs
@@ -2,10 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Kbry.Mobile.Services
 {
     public interface IRegistrationRepository
     {
+        void ClearFileAsync();
+        Task<RegistrationCode> GetRegistrationAsync();
+        void AddRegistration(RegistrationCode registration);
     }
 }
--- Kbry.Mobile/Kbry.Mobile/Services/RegistrationRepository.cs
+++ Kbry.Mobile/Kbry.Mobile/Services/RegistrationRepository.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Kbry.Mobile.Models;
+using Newtonsoft.Json;
 using PCLStorage;
 
 namespace Kbry.Mobile.Services
 {
     public class RegistrationRepository : IRegistrationRepository
     {
-        private readonly IFolder _rootFolder;
         private readonly IFolder _folder;
         private readonly IFile _file;
 
         public RegistrationRepository()
         {
-            _rootFolder = FileSystem.Current.LocalStorage;
-            _folder = FileHelper.GetOrCreateFolderAsync(_rootFolder, "registration").Result;
+            var rootFolder = FileSystem.Current.LocalStorage;
+            _folder = FileHelper.GetOrCreateFolderAsync(rootFolder, "registration").Result;
             _file = FileHelper.GetOrCreateFileAsync(_folder, "registration").Result;
+        }
+
+        public async void ClearFileAsync()
+        {
+            await _folder.DeleteAsync();
+        }
+
+        public async Task<RegistrationCode> GetRegistrationAsync()
+        {
+            var json = await _file.ReadAllTextAsync();
+            return JsonConvert.DeserializeObject<RegistrationCode>(json);
+        }
+
+        public async void AddRegistration(RegistrationCode registration)
+        {
+            await _file.WriteAllTextAsync(JsonConvert.SerializeObject(registration));
         }
     }
 }
