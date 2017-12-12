348d3d7e4022bf6549dc9804c98b65bd5e42643c
danieljaller <danieljaller@gmail.com>
2017-12-12 11:43:56 +0100

    Redid everything, cutting and pasting from different branches to get the nuget-packages to work with each other.

---
+++ Kbry.Web/Migrations/201712111419338_InitialDatabaseCreation.cs
@@ -0,0 +1,16 @@
+namespace Kbry.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDatabaseCreation : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Kbry.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kbry.Web.Startup))]
+namespace Kbry.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
