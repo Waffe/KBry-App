4b9a4711816f73933b515fc3b390725b71c3e3e2
danieljaller <danieljaller@gmail.com>
2018-01-15 14:21:33 +0100

    Removed the old projects.

--- Kbry.Mobile/Kbry.Mobile/Models/RegistrationCode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kbry.Mobile.Models
-{
-    public class RegistrationCode
-    {
-        public int Id { get; set; }
-        
-        public string RegistrationString { get; set; }
-    }
-}
