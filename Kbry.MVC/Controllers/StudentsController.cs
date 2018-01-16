2edc3b29439e71ab952067ce61b9e2245f936a3d
danieljaller <danieljaller@gmail.com>
2018-01-16 10:12:42 +0100

    Experimenting with datepicker. Doesn't work yet.

--- Kbry.MVC/Controllers/StudentsController.cs
+++ Kbry.MVC/Controllers/StudentsController.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Core.Common.CommandTrees;
-using System.Data.Entity.Core.Objects;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
---
+++ Kbry.MVC/Scripts/scripts.js
@@ -0,0 +1,1 @@
+﻿$(document).ready(function() { $('input[name="daterange"]').daterangepicker(); })
