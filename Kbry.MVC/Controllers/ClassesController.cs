1ca05ade11b03a2632874f25f128635e2d7f4bac
danieljaller <danieljaller@gmail.com>
2018-01-18 11:43:50 +0100

    It works!!

--- Kbry.MVC/Controllers/ClassesController.cs
+++ Kbry.MVC/Controllers/ClassesController.cs
@@ -1,124 +1,123 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Kbry.Data;
 using Kbry.Data.Model;
+using Kbry.MVC.ViewModels;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 
 namespace Kbry.MVC.Controllers
 {
     public class ClassesController : Controller
     {
-        private KbryDbContext db = new KbryDbContext();
+        private readonly KbryDbContext _db = new KbryDbContext();
 
         // GET: Classes
         public ActionResult Index()
         {
-            return View(db.Classes.ToList());
-        }
-
-        // GET: Classes/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            SchoolClass schoolClass = db.Classes.Find(id);
-            if (schoolClass == null)
-            {
-                return HttpNotFound();
-            }
-            return View(schoolClass);
-        }
-
-        // GET: Classes/Create
-        public ActionResult Create()
-        {
             return View();
-        }
-
-        // POST: Classes/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name")] SchoolClass schoolClass)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Classes.Add(schoolClass);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(schoolClass);
-        }
-
-        // GET: Classes/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            SchoolClass schoolClass = db.Classes.Find(id);
-            if (schoolClass == null)
-            {
-                return HttpNotFound();
-            }
-            return View(schoolClass);
-        }
-
-        // POST: Classes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name")] SchoolClass schoolClass)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(schoolClass).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(schoolClass);
-        }
-
-        // GET: Classes/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            SchoolClass schoolClass = db.Classes.Find(id);
-            if (schoolClass == null)
-            {
-                return HttpNotFound();
-            }
-            return View(schoolClass);
-        }
-
-        // POST: Classes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            SchoolClass schoolClass = db.Classes.Find(id);
-            db.Classes.Remove(schoolClass);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
+                _db.Dispose();
             }
             base.Dispose(disposing);
         }
+
+        public async Task<ActionResult> GetSchoolClasses([DataSourceRequest]DataSourceRequest request)
+        {
+            var allClasses = await _db.Classes.Include(c => c.Students).ToListAsync();
+            var schoolClassViewModels = new List<SchoolClassViewModel>();
+            foreach (var schoolClass in allClasses)
+            {
+                schoolClassViewModels.Add(ConvertToViewModel(schoolClass));
+            }
+            return Json(schoolClassViewModels.ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Create([DataSourceRequest] DataSourceRequest request, SchoolClassViewModel schoolClass)
+        {
+            if (schoolClass != null && ModelState.IsValid)
+            {
+                var classToAdd = ConvertToModel(schoolClass);
+                _db.Classes.Add(classToAdd);
+                _db.SaveChanges();
+            }
+
+            return Json(new[] { schoolClass }.ToDataSourceResult(request, ModelState));
+        }
+
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Update([DataSourceRequest] DataSourceRequest request, SchoolClassViewModel schoolClass)
+        {
+            if (schoolClass != null && ModelState.IsValid)
+            {
+                var edited = _db.Classes.FirstOrDefault(s => s.Id == schoolClass.Id);
+                edited.Id = schoolClass.Id;
+                edited.Name = schoolClass.Name;
+                _db.Classes.AddOrUpdate(edited);
+                _db.SaveChanges();
+            }
+
+            return Json(new[] { schoolClass }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Destroy([DataSourceRequest] DataSourceRequest request, SchoolClassViewModel schoolClass)
+        {
+            if (schoolClass != null)
+            {
+                var schoolClassToRemove = _db.Classes.FirstOrDefault(s => s.Id == schoolClass.Id);
+                if (schoolClassToRemove != null)
+                {
+                    _db.Classes.Remove(schoolClassToRemove);
+                    _db.SaveChanges();
+                }
+            }
+
+            return Json(new[] { schoolClass }.ToDataSourceResult(request, ModelState));
+        }
+        private SchoolClass ConvertToModel(SchoolClassViewModel schoolClass)
+        {
+            return new SchoolClass()
+            {
+                Id = schoolClass.Id,
+                Name = schoolClass.Name
+            };
+        }
+
+        private IEnumerable<SchoolClassViewModel> ConvertManyToViewModel(IEnumerable<SchoolClass> schoolClasses)
+        {
+            var schoolClassViewModels = new List<SchoolClassViewModel>();
+
+            foreach (var schoolClass in schoolClasses)
+            {
+                schoolClassViewModels.Add(ConvertToViewModel(schoolClass));
+            }
+
+            return schoolClassViewModels;
+        }
+
+        private SchoolClassViewModel ConvertToViewModel(SchoolClass model)
+        {
+            return new SchoolClassViewModel()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                NumberOfStudents = model.Students.ToList().Count
+            };
+        }
     }
+
 }
---
+++ Kbry.MVC/ViewModels/SchoolClassViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kbry.Data.Model;
+
+namespace Kbry.MVC.ViewModels
+{
+    public class SchoolClassViewModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Display(Name = "Class Name")]
+        public string Name { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        [Display(Name = "Number of Students")]
+        public int NumberOfStudents { get; set; }
+    }
+}
